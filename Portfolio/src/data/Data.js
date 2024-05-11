7b80324035614ede8498e94c4884ea89ee97f072
Prerna-09 <prerna09sijwali@gmail.com>
2024-05-11 11:31:10 +0530

    added files

--- Portfolio/src/data/Data.js
+++ Portfolio/src/data/Data.js
@@ -1,19 +1,17 @@
 export const Bio = {
     name: "Prerna Sijwali",
     roles: [
+      "Student",
       "Frontent Developer",
-      "UI/UX Designer",
       "Programmer",
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
+    github: "https://github.com/Prerna-09",
+   
+    
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-    twitter: "https://twitter.com/RishavChanda",
-    insta: "https://www.instagram.com/rishav_chanda/",
-    facebook: "https://www.facebook.com/rishav.chanda.165/",
+      "j",
   };
   
   export const skills = [
